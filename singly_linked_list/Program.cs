0eb391946dabb5dba3eaaad87e80c2a1879eb701
rizqiindah03 <rizqiindah32@gmail.com>
2022-11-11 17:12:45 +0700

    Add project files.

---
+++ singly_linked_list/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singly_linked_list
+{
+    //each node consist of the information part and link to the next node
+    class node
+    {
+        public int rollNumber;
+        public string name;
+        public node next;      
+    }
+   
+    }
+}
